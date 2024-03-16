e2026f748a1eabd435ba672ea40a15f0756d8d71
Valentin Marta <valenmarta17@gmail.com>
2024-03-16 19:13:11 -0300

    Creating the menu

---
+++ main.py
@@ -0,0 +1,25 @@
+def main():
+
+    while True:
+    
+        op = input("""------> Library managment <------
+        1.   Create books.
+        2.   Show books that are available.
+        3.   Change the availability of the books (rented or returned).
+        4.   Exit.
+        Option: """)
+
+        if op == "1":
+            pass
+        elif op == "2":
+            pass
+        elif op == "3":
+            pass
+        elif op == "4":
+            break
+        else:
+            print("Invalid value. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
